782ba0ce787c249ddc6d2236fbda2a30f629f423
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-13 21:36:18 +0900

    refactor: 변수 명 수정

--- backend/src/auth/utils/user.decorator.ts
+++ backend/src/auth/utils/user.decorator.ts
@@ -1,6 +1,6 @@
 import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 
-export const User = createParamDecorator((data: unknown, ctx: ExecutionContext) => {
-  const request = ctx.switchToHttp().getRequest();
+export const User = createParamDecorator((data: unknown, executionContext: ExecutionContext) => {
+  const request = executionContext.switchToHttp().getRequest();
   return request.user;
 });
