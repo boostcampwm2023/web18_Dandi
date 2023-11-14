48d94dcdaa8106703734ba75a8d19643e2afd845
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-14 16:25:01 +0900

    build: jwt 의존성 추가

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -3,8 +3,15 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { NaverStrategy } from './strategy/naverAuth.strategy';
 import { UserRepository } from './auth.repository';
+import { JwtModule } from '@nestjs/jwt';
+import 'dotenv/config';
 
 @Module({
+  imports: [
+    JwtModule.register({
+      secret: process.env.JWT_SECRET,
+    }),
+  ],
   controllers: [AuthController],
   providers: [AuthService, UserRepository, NaverStrategy],
 })
