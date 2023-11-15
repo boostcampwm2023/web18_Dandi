096ff0caeda001bcf9e15dbb9f6d7136442524d1
최수현 <sw92522@naver.com>
2023-11-16 00:22:28 +0900

    refactor: jwt의 만료기간 설정 #8
    jwt의 만료기간을 cookie의 maxAge를 이용해 설정하는 것에서 jwt 자체에
    만료 기간을 부여하는 것으로 변경함.

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -24,17 +24,21 @@ export class AuthService {
       user = await this.signUp(createUserDto);
     }
 
-    // refresh token 기간 2주로 설정
+    // refresh token 만료기간 2주로 설정
     const dataForRefresh = {socialType: createUserDto.socialType, refreshToken: createUserDto.refreshToken};
     this.redis.set(`${user.id}`, JSON.stringify(dataForRefresh), 'EX', 60*60*24*14);
 
     return {
       userId: user.id,
-      token: this.jwtService.sign({
-        id: user.id,
-        nickname: user.nickname,
-        accessToken: createUserDto.accessToken,
-      }),
+      token: this.jwtService.sign(
+        {
+          id: user.id,
+          nickname: user.nickname,
+          accessToken: createUserDto.accessToken,
+        },
+        // jwt 만료기간 30분으로 설정
+        { expiresIn: `${60*30*1000}` }
+      ),
     };
   }
 
