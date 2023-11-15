c48130f6738b2681d8f4dd11e96573f47da0804b
최수현 <sw92522@naver.com>
2023-11-16 04:17:33 +0900

    feat: 사용자 인가 여부 확인하는 AuthGuard 생성 #8

---
+++ backend/src/auth/guards/auth.guard.ts
@@ -0,0 +1,32 @@
+import { CanActivate, ExecutionContext, Injectable, UnauthorizedException } from "@nestjs/common";
+import { JwtService } from "@nestjs/jwt";
+
+@Injectable()
+export class AuthGuard implements CanActivate {
+  constructor(private jwtService: JwtService) {}
+
+  async canActivate(context: ExecutionContext): Promise<boolean> {
+    const req = context.switchToHttp().getRequest();
+    const token = this.extractTokenFromHeader(req);
+    if (!token) {
+      throw new UnauthorizedException();
+    }
+    try {
+      const payload = await this.jwtService.verifyAsync(
+        token,
+        {
+          secret: process.env.JWT_SECRET
+        }
+      );
+      req['user'] = payload;
+    } catch {
+      throw new UnauthorizedException();
+    }
+    return true;
+  }
+
+  private extractTokenFromHeader(request: Request): string | undefined {
+    const [type, token] = request.headers['authorization'].split(' ') ?? [];
+    return type === 'Bearer' ? token : undefined;
+  }
+}
