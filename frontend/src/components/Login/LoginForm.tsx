84082794786106f0c0fc1632d13986836dbfda11
unknown <speed04313@gmail.com>
2023-11-16 13:20:32 +0900

    design: CSS 일부 수정 #38
    LoginForm의 gap 수정
    네이버 로그인 버튼의 border radius 수정

--- frontend/src/components/Login/LoginForm.tsx
+++ frontend/src/components/Login/LoginForm.tsx
@@ -4,11 +4,11 @@ import naverLogin from '../../assets/image/naverLogin.svg';
 
 const LoginForm = () => {
   return (
-    <div className="border-default relative flex  w-1/4 flex-col items-center justify-center gap-5 rounded-2xl border bg-[#fff] py-16 shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
+    <div className="border-default relative flex  w-1/4 flex-col items-center justify-center gap-9 rounded-2xl bg-[#fff] py-16 shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
       <img src={logo} alt="logo" className="w-1/4" />
-      <span className="font-bold text-[#000]">SNS로 빠르게 단디 시작하기</span>
+      <p className="font-bold text-[#000]">SNS로 빠르게 단디 시작하기</p>
       <button className="flex w-1/2 justify-center">
-        <img src={naverLogin} alt="login" className="w-4/5" />
+        <img src={naverLogin} alt="login" className="w-4/5 rounded-lg" />
       </button>
     </div>
   );
