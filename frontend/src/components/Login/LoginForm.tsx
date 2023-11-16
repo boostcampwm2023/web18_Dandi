eb56ae92221efb0e926f2d79279a0f375c7c72b8
unknown <speed04313@gmail.com>
2023-11-16 13:08:02 +0900

    design: LoginForm CSS 일부 수정
    LoginForm height 값을 주지 않고 padding 값으로 조정
    로그인 버튼 사이즈 조정

--- frontend/src/components/Login/LoginForm.tsx
+++ frontend/src/components/Login/LoginForm.tsx
@@ -4,11 +4,11 @@ import naverLogin from '../../assets/image/naverLogin.svg';
 
 const LoginForm = () => {
   return (
-    <div className="border-default relative flex h-60 w-1/4 flex-col items-center justify-center gap-5 rounded-2xl border bg-[#fff] p-4 shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
+    <div className="border-default relative flex  w-1/4 flex-col items-center justify-center gap-5 rounded-2xl border bg-[#fff] py-16 shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
       <img src={logo} alt="logo" className="w-1/4" />
       <span className="font-bold text-[#000]">SNS로 빠르게 단디 시작하기</span>
-      <button className="w-1/2 items-center">
-        <img src={naverLogin} alt="login" />
+      <button className="flex w-1/2 justify-center">
+        <img src={naverLogin} alt="login" className="w-4/5" />
       </button>
     </div>
   );
