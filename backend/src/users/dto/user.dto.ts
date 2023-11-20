5ea147c166514906476b2dc00639d7c1d19adb3c
최수현 <sw92522@naver.com>
2023-11-20 15:56:23 +0900

    feat: 사용자 검색 response dto 생성 #58

---
+++ backend/src/users/dto/user.dto.ts
@@ -0,0 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
+export class searchUserResponseDto {
+  @ApiProperty({ description: '사용자 ID' })
+  id: number;
+
+  @ApiProperty({ description: '사용자 닉네임' })
+  nickname: string;
+
+  @ApiProperty({ description: '사용자 프로필 이미지' })
+  profile_image: string;
+}
