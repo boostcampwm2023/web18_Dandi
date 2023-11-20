3d8edc8d23dac25ebdbd8cd8269ffcd3e01f9efa
최수현 <sw92522@naver.com>
2023-11-20 17:42:17 +0900

    chore: swagger annotations 작성 #58

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,15 +1,18 @@
 import { Controller, Get, Param, UseGuards } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwtAuth.guard';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { SearchUserResponseDto } from './dto/user.dto';
 
+@ApiTags('Users API')
 @Controller('users')
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
   @Get('/search/:nickname')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ description: '닉네임으로 사용자 검색 API' })
+  @ApiOkResponse({ description: '닉네임을 사용자 검색 성공', type: Array<SearchUserResponseDto> })
   async searchUsers(@Param('nickname') nickname: string): Promise<SearchUserResponseDto[]> {
     return this.usersService.searchUsers(nickname);
   }
