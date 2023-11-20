bb34dc43d918bded80ad1e5ea320eb08fb665a84
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-20 16:45:05 +0900

    chore: 스웨거 데코레이터 변경 boostcamp#54

--- backend/src/diaries/diaries.controller.ts
+++ backend/src/diaries/diaries.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Post, UseGuards, UsePipes, ValidationPipe } from '@nestjs/common';
-import { ApiBody, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateDiaryDto } from './dto/diary.dto';
 import { DiariesService } from './diaries.service';
 import { User as UserEntity } from 'src/users/entity/user.entity';
@@ -15,7 +15,7 @@ export class DiariesController {
   @UseGuards(JwtAuthGuard)
   @UsePipes(ValidationPipe)
   @ApiOperation({ description: '일기 저장 API' })
-  @ApiOkResponse({ description: '일기 저장 성공' })
+  @ApiCreatedResponse({ description: '일기 저장 성공' })
   @ApiBody({ type: CreateDiaryDto })
   async createDiary(@User() user: UserEntity, @Body() createDiaryDto: CreateDiaryDto) {
     await this.diariesService.saveDiary(user, createDiaryDto);
