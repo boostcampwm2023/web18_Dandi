a1e01836c04be7a0eecd65eaa93549789d539cc6
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-20 20:30:52 +0900

    fix: 환경 변수 명 수정 boostcampwm2023#77

--- backend/src/configs/typeorm.config.ts
+++ backend/src/configs/typeorm.config.ts
@@ -3,11 +3,11 @@ import 'dotenv/config';
 
 export const typeORMDevConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST_DEV,
-  port: Number(process.env.DB_PORT_DEV),
-  username: process.env.DB_USER_DEV,
-  password: process.env.DB_PASS_DEV,
-  database: process.env.DB_NAME_DEV,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
   synchronize: true,
   logging: true,
@@ -15,10 +15,10 @@ export const typeORMDevConfig: TypeOrmModuleOptions = {
 
 export const typeORMProdConfig: TypeOrmModuleOptions = {
   type: 'mysql',
-  host: process.env.DB_HOST_PROD,
-  port: Number(process.env.DB_PORT_PROD),
-  username: process.env.DB_USER_PROD,
-  password: process.env.DB_PASS_PROD,
-  database: process.env.DB_NAME_PROD,
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
   entities: [__dirname + '/../**/*.entity.{js,ts}'],
 };
