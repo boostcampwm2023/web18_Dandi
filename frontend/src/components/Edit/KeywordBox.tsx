8c8e5615beb73c06681e114fb9e2b303ba71e489
이도훈 <qwer2854@naver.com>
2023-11-20 16:07:29 +0900

    feat: 키워드 입력 구현 #72
    키워드를 입력받고 입력받은 키워드를 해시태그형식으로 출력

---
+++ frontend/src/components/Edit/KeywordBox.tsx
@@ -0,0 +1,46 @@
+import KeywordItem from '@components/Edit/KeywordItem';
+
+import { useState } from 'react';
+
+interface KeywordBoxProps {
+  keywordList: string[];
+  setKeywordList: React.Dispatch<React.SetStateAction<string[]>>;
+}
+
+const KeywordBox = ({ keywordList, setKeywordList }: KeywordBoxProps) => {
+  const [keyword, setKeyword] = useState<string>('');
+
+  const changeKeyword = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setKeyword(e.target.value);
+  };
+
+  const addKeyword = (e: React.KeyboardEvent<HTMLInputElement>) => {
+    if (e.key === 'Enter') {
+      setKeywordList((pre) => [...pre, keyword]);
+      setKeyword('');
+    }
+  };
+
+  return (
+    <div className="flex w-[80%] flex-col ">
+      <div className="flex w-full justify-between">
+        <label className="mb-3 text-xl font-bold" htmlFor="keyword">
+          키워드 입력
+        </label>
+        <button className="bg-brown rounded-lg px-3 text-lg font-bold">저장하기</button>
+      </div>
+
+      <input
+        type="text"
+        id="keyword"
+        value={keyword}
+        onChange={changeKeyword}
+        onKeyPress={addKeyword}
+        className="border-brown mb-3 h-[2.5rem]  w-[30%] rounded-xl border-[1px] pl-4 outline-none"
+      />
+      <KeywordItem keywordList={keywordList} />
+    </div>
+  );
+};
+
+export default KeywordBox;
---
+++ frontend/src/components/Edit/KeywordItem.tsx
@@ -0,0 +1,20 @@
+interface KeywordItemProps {
+  keywordList: string[];
+}
+
+const KeywordItem = ({ keywordList }: KeywordItemProps) => {
+  return (
+    <div className="flex flex-wrap gap-4">
+      {keywordList.map((keyword, index) => (
+        <div
+          className="bg-mint flex h-9 items-center justify-center rounded-lg  px-3 text-base text-[black]"
+          key={index}
+        >
+          #{keyword}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default KeywordItem;
