3b08f67981a074ff2d86a63f583e0ee95ab7902c
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-20 19:56:41 +0900

    chore: 응답 dto에 swagger 데코레이터 추가 boostcampwb#57

--- backend/src/diaries/dto/diary.dto.ts
+++ backend/src/diaries/dto/diary.dto.ts
@@ -32,22 +32,31 @@ export class CreateDiaryDto {
   status: DiaryStatus;
 }
 
-export interface GetDiaryResponseDto {
+export class GetDiaryResponseDto {
+  @ApiProperty({ description: '작성자 ID' })
   userId: number;
 
+  @ApiProperty({ description: '작성자 닉네임' })
   authorName: string;
 
+  @ApiProperty({ description: '일기 제목' })
   title: string;
 
+  @ApiProperty({ description: '일기 내용' })
   content: string;
 
+  @ApiProperty({ description: '일기 썸네일' })
   thumbnail: string;
 
+  @ApiProperty({ description: '감정(이모지)' })
   emotion: string;
 
+  @ApiProperty({ description: '사용자의 기분(a ~ b 사이의 실수 값)' })
   mood: number;
 
+  @ApiProperty({ description: '일기 태그 배열' })
   tags: string[];
 
+  @ApiProperty({ description: '해당 글의 리액션 갯수' })
   reactionCount: number;
 }
