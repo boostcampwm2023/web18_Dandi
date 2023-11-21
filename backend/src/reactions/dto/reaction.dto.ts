51bdf896bf83a237e6a175a5e14dc50db14fdb9a
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-21 21:56:58 +0900

    feat: 응답에 대한 dto 타입 정의

--- backend/src/reactions/dto/reaction.dto.ts
+++ backend/src/reactions/dto/reaction.dto.ts
@@ -4,3 +4,14 @@ export class CreateReactionDto {
   @IsNotEmpty()
   reaction: string;
 }
+
+export class GetReactionResponseDto {
+  reactionList: ReactionInfo[];
+}
+
+export class ReactionInfo {
+  userId: number;
+  nickname: string;
+  profileImage: string;
+  reaction: string;
+}
--- backend/src/reactions/reactions.controller.ts
+++ backend/src/reactions/reactions.controller.ts
@@ -13,7 +13,7 @@ import { ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { User as UserEntity } from 'src/users/entity/user.entity';
 import { User } from 'src/users/utils/user.decorator';
 import { JwtAuthGuard } from 'src/auth/guards/jwtAuth.guard';
-import { CreateReactionDto } from './dto/reaction.dto';
+import { CreateReactionDto, GetReactionResponseDto, ReactionInfo } from './dto/reaction.dto';
 import { ReactionsService } from './reactions.service';
 
 @ApiTags('Reaction API')
@@ -26,10 +26,13 @@ export class ReactionsController {
   @UsePipes(ValidationPipe)
   @ApiOperation({ description: '리액션 조회 API' })
   @ApiCreatedResponse({ description: '리액션 조회 성공' })
-  async getReactions(@User() user: UserEntity, @Param('diaryId', ParseIntPipe) diaryId: number) {
+  async getReactions(
+    @User() user: UserEntity,
+    @Param('diaryId', ParseIntPipe) diaryId: number,
+  ): Promise<GetReactionResponseDto> {
     const reactions = await this.reactionsService.getReaction(user, diaryId);
 
-    return reactions.map((reaction) => {
+    const reactionList = reactions.map<ReactionInfo>((reaction) => {
       return {
         userId: reaction.user.id,
         nickname: reaction.user.nickname,
@@ -37,6 +40,7 @@ export class ReactionsController {
         reaction: reaction.reaction,
       };
     });
+    return { reactionList };
   }
 
   @Post('/:diaryId')
