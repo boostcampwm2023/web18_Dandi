55cf38bb9136dc049432fe63bd67bcbfad270daf
최수현 <sw92522@naver.com>
2023-11-21 22:44:30 +0900

    feat: 예외처리 추가 #63

--- backend/src/friends/friends.service.ts
+++ backend/src/friends/friends.service.ts
@@ -16,9 +16,15 @@ export class FriendsService {
     const { senderId, receiverId } = friendRelationDto;
 
     // 예외처리
+    if (senderId === receiverId) throw new BadRequestException('나에게 친구신청 보낼 수 없습니다.');
+
     const relations = await this.friendsRepository.findFriendRequest(senderId, receiverId);
     if (relations.length > 0) throw new BadRequestException('이미 친구신청을 하셨습니다.');
-    if (senderId === receiverId) throw new BadRequestException('나에게 친구신청 보낼 수 없습니다.');
+
+    const reverseRelations = await this.friendsRepository.findFriendRequest(receiverId, senderId);
+    if (reverseRelations.length > 0) {
+      throw new BadRequestException('상대의 친구신청을 확인해주세요.');
+    }
 
     const sender = await this.usersRepository.findById(senderId);
     const receiver = await this.usersRepository.findById(receiverId);
@@ -35,19 +41,31 @@ export class FriendsService {
     this.friendsRepository.updateStatus(friendRequest);
   }
 
-  // 예외처리(친구 신청 제외한 로직)
+  // 예외처리(친구 신청 제외한 로직 : 신청 취소, 신청 수락, 신청 거절)
   private async checkFriendData(friendRelationDto: FriendRelationDto): Promise<Friend> {
     const { senderId, receiverId } = friendRelationDto;
     if (senderId === receiverId)
       throw new BadRequestException('나와는 친구신청 관리를 할 수 없습니다.');
 
-    const relation = await this.friendsRepository.findFriendRequest(senderId, receiverId);
-    if (relation.length !== 1)
-      throw new BadRequestException('해당 사용자 사이의 친구신청 기록이 없습니다.');
+    const relations = await this.friendsRepository.findFriendRequest(senderId, receiverId);
 
-    if (relation[0].status === FriendStatus.COMPLETE) {
-      throw new BadRequestException('이미 수락된 친구신청입니다.');
+    if (relations.length < 1) {
+      const reverseRelations = await this.friendsRepository.findFriendRequest(receiverId, senderId);
+
+      if (reverseRelations) {
+        if (reverseRelations[0].status === FriendStatus.COMPLETE) {
+          throw new BadRequestException('이미 친구입니다.');
+        }
+        throw new BadRequestException('상대의 친구신청을 확인하세요.');
+      } else {
+        throw new BadRequestException('해당 사용자 사이의 친구신청 기록이 없습니다.');
+      }
     }
-    return relation[0];
+
+    if (relations[0].status === FriendStatus.COMPLETE) {
+      throw new BadRequestException('이미 친구입니다.');
+    }
+
+    return relations[0];
   }
 }
