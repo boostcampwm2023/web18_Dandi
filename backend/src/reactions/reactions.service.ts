f4a984c4a9a32787bedbca3f6ca65759aa5f721a
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-22 11:14:02 +0900

    feat: 리액션 삭제 기능 구현 #110

--- backend/src/reactions/reactions.service.ts
+++ backend/src/reactions/reactions.service.ts
@@ -1,7 +1,7 @@
 import { BadRequestException, Injectable } from '@nestjs/common';
 import { ReactionsRepository } from './reactions.repository';
 import { User } from 'src/users/entity/user.entity';
-import { CreateReactionDto } from './dto/reaction.dto';
+import { ReactionRequestDto } from './dto/reaction.dto';
 import { DiariesService } from 'src/diaries/diaries.service';
 
 @Injectable()
@@ -11,24 +11,39 @@ export class ReactionsService {
     private readonly diariesService: DiariesService,
   ) {}
 
-  async getReaction(user: User, diaryId: number) {
+  async getAllReaction(user: User, diaryId: number) {
     const diary = await this.diariesService.findDiary(user, diaryId, true);
 
     return await this.reactionsRepository.findByDiary(diary);
   }
 
-  async saveReaction(user: User, diaryId: number, createReactionDto: CreateReactionDto) {
+  async saveReaction(user: User, diaryId: number, reactionRequestDto: ReactionRequestDto) {
     const diary = await this.diariesService.findDiary(user, diaryId, true);
 
     const duplicateReaction = await this.reactionsRepository.findReactionByDiaryAndUserAndReaction(
       user,
       diary,
-      createReactionDto.reaction,
+      reactionRequestDto.reaction,
     );
     if (duplicateReaction) {
       throw new BadRequestException('이미 저장된 리액션 정보입니다.');
     }
 
-    this.reactionsRepository.save({ user, diary, reaction: createReactionDto.reaction });
+    this.reactionsRepository.save({ user, diary, reaction: reactionRequestDto.reaction });
+  }
+
+  async deleteReaction(user: User, diaryId: number, reactionRequestDto: ReactionRequestDto) {
+    const diary = await this.diariesService.findDiary(user, diaryId, true);
+
+    const reaction = await this.reactionsRepository.findReactionByDiaryAndUserAndReaction(
+      user,
+      diary,
+      reactionRequestDto.reaction,
+    );
+    if (!reaction) {
+      throw new BadRequestException('이미 삭제된 리액션 정보입니다.');
+    }
+
+    this.reactionsRepository.remove(reaction);
   }
 }
