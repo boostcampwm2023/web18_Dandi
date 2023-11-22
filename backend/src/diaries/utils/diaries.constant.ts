3d723c018e664a699dcb3b6b4b53e80f5f4b6c63
최수현 <sw92522@naver.com>
2023-11-22 18:30:44 +0900

    feat: mood type 추가 #114

--- backend/src/diaries/utils/diaries.constant.ts
+++ backend/src/diaries/utils/diaries.constant.ts
@@ -1,15 +1,16 @@
 export const CLOVA_SENTIMENT_URL =
   'https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze';
 
-export enum SentimentHeaders {
-  NCP_API_ID = 'X-NCP-APIGW-API-KEY-ID',
-  NCP_API_SECRET = 'X-NCP-APIGW-API-KEY',
+export enum MoodType {
+  POSITIVE = 'positive',
+  NEUTRAL = 'neutral',
+  NEGATIVE = 'negative',
 }
 
 export enum MoodDegree {
-  '매우 부정' = 1,
-  '부정' = 2,
-  '보통' = 3,
-  '긍정' = 4,
-  '매우 긍정' = 5,
+  BAD = 1,
+  SO_BAD = 2,
+  SO_SO = 3,
+  GOOD = 4,
+  SO_GOOD = 5,
 }
