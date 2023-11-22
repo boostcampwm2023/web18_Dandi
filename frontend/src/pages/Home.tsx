fa3566186a4c6f79198ff82bda745cfcd67683f8
Seo Jonghyeon <86141652+surpise@users.noreply.github.com>
2023-11-22 16:17:58 +0900

    Revert "[FE] 잔디 UI 구성"

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,12 +1,9 @@
 import DiaryList from '@components/Common/DiaryList';
 import NavBar from '@components/Common/NavBar';
 import Profile from '@components/Home/Profile';
-import Grass from '@components/Home/Grass';
-
 import { dummyData } from '../util/dummyData';
 
 const Home = () => {
-
   return (
     <main className="mb-28 flex flex-col items-center justify-start">
       <NavBar />
@@ -16,7 +13,6 @@ const Home = () => {
         totalFriends={10}
         profileImage="https://i.namu.wiki/i/VMIHkLm6DcUT4d9-vN4yFw7Yfitr8luT_U2YwJsugGodCQ01ooGH_kHX0D6sJ3HDS1YHfvy9B81al8rKCxqKYw.webp"
       />
-      <Grass />
       <DiaryList pageType="home" diaryData={dummyData} />
     </main>
   );
