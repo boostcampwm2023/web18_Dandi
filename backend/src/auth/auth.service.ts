b99c91bf16942cb3f497e6db69feb4245e4629ab
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-22 17:28:48 +0900

    refactor: 공통 함수 분리 #120

--- backend/src/auth/auth.service.ts
+++ backend/src/auth/auth.service.ts
@@ -45,6 +45,34 @@ export class AuthService {
     return await this.usersRepository.createUser(user, socialType);
   }
 
+  private async getToken(oAuthLoginDto: OAuthLoginDto) {
+    const response = await fetch('https://nid.naver.com/oauth2.0/token', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: this.makeNaverOauthParam(oAuthLoginDto),
+    });
+
+    const data = await response.json();
+    return {
+      accessToken: data.access_token,
+      refreshToken: data.refresh_token,
+    };
+  }
+
+  private async getUserProfile(accessToken: string) {
+    const response = await fetch('https://openapi.naver.com/v1/nid/me', {
+      method: 'GET',
+      headers: {
+        Authorization: 'Bearer ' + accessToken,
+      },
+    });
+
+    const data = await response.json();
+    return data.response as AuthUserDto;
+  }
+
   async refreshAccessToken(req: Request) {
     const userJwt = cookieExtractor(req);
     const payload = this.jwtService.decode(userJwt);
@@ -55,11 +83,10 @@ export class AuthService {
       headers: {
         'Content-Type': 'application/x-www-form-urlencoded',
       },
-      body: this.makeNaverRefreshParams(refreshTokenData['refreshToken']),
+      body: this.makeNaverOauthParam(refreshTokenData['refreshToken']),
     });
 
     const jsonData = await newData.json();
-
     return this.jwtService.sign({
       id: payload.id,
       nickname: payload.nickname,
@@ -67,48 +94,20 @@ export class AuthService {
     });
   }
 
-  private makeNaverRefreshParams(refreshToken: string): URLSearchParams {
+  private makeNaverOauthParam(paramData: string | OAuthLoginDto): URLSearchParams {
     const params = new URLSearchParams();
-    params.append('grant_type', 'refresh_token');
     params.append('client_id', process.env.NAVER_CLIENT_ID);
     params.append('client_secret', process.env.NAVER_CLIENT_SECRET);
-    params.append('refresh_token', refreshToken);
+
+    if (paramData instanceof OAuthLoginDto) {
+      params.append('grant_type', 'authorization_code');
+      params.append('code', paramData.code);
+      params.append('state', paramData.state);
+    } else {
+      params.append('grant_type', 'refresh_token');
+      params.append('refresh_token', paramData);
+    }
 
     return params;
-  }
-
-  async getToken(oAuthLoginDto: OAuthLoginDto) {
-    const params = new URLSearchParams();
-    params.append('grant_type', 'authorization_code');
-    params.append('client_id', process.env.NAVER_CLIENT_ID);
-    params.append('client_secret', process.env.NAVER_CLIENT_SECRET);
-    params.append('code', oAuthLoginDto.code);
-    params.append('state', oAuthLoginDto.state);
-
-    const response = await fetch('https://nid.naver.com/oauth2.0/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-      },
-      body: params,
-    });
-
-    const data = await response.json();
-    return {
-      accessToken: data.access_token,
-      refreshToken: data.refresh_token,
-    };
-  }
-
-  async getUserProfile(accessToken: string) {
-    const response = await fetch('https://openapi.naver.com/v1/nid/me', {
-      method: 'GET',
-      headers: {
-        Authorization: 'Bearer ' + accessToken,
-      },
-    });
-
-    const data = await response.json();
-    return data.response as AuthUserDto;
   }
 }
