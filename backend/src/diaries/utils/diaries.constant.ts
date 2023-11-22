3eeb50eb11e75faff3f69a1d22956a6247eda205
최수현 <sw92522@naver.com>
2023-11-22 16:38:43 +0900

    feat: CLOVA Sentiment에 필요한 값 세팅 #114
    - 환경변수, 상수 저장

---
+++ backend/src/diaries/utils/diaries.constant.ts
@@ -0,0 +1,7 @@
+export const CLOVA_SENTIMENT_URL =
+  'https://naveropenapi.apigw.ntruss.com/sentiment-analysis/v1/analyze';
+
+export enum SentimentHeaders {
+  NCP_API_ID = 'X-NCP-APIGW-API-KEY-ID',
+  NCP_API_SECRET = 'X-NCP-APIGW-API-KEY',
+}
