606f21aa80b84b2f36a18f889a3c1e31920e2a55
rio <75190035+HyoJongPark@users.noreply.github.com>
2023-11-23 14:34:29 +0900

    chore: N+1 문제 수정 #100

--- backend/src/diaries/diaries.repository.ts
+++ backend/src/diaries/diaries.repository.ts
@@ -1,7 +1,7 @@
 import { DataSource, Repository } from 'typeorm';
 import { Diary } from './entity/diary.entity';
 import { Injectable } from '@nestjs/common';
-import { GetAllEmotionsResponseDto, ReadUserDiariesRequestDto } from './dto/diary.dto';
+import { ReadUserDiariesRequestDto } from './dto/diary.dto';
 
 const ITEM_PER_PAGE = 5;
 
@@ -19,12 +19,14 @@ export class DiariesRepository extends Repository<Diary> {
     });
   }
 
-  async findDiaryByAuthorIdWithPagination(
+  async findDiariesByAuthorIdWithPagination(
     authorId: number,
     isOwner: boolean,
     requestDto: ReadUserDiariesRequestDto,
   ) {
     const queryBuilder = this.createQueryBuilder('diary')
+      .leftJoin('diary.tags', 'tags')
+      .leftJoin('diary.reactions', 'reactions')
       .where('diary.author.id = :authorId', {
         authorId,
       })
@@ -35,7 +37,27 @@ export class DiariesRepository extends Repository<Diary> {
     if (!isOwner) {
       queryBuilder.andWhere('diary.status = :status', { status: 'public' });
     }
-    return await queryBuilder.getRawMany();
+    return await queryBuilder.getMany();
+  }
+
+  async findDiariesByAuthorIdWithDates(
+    authorId: number,
+    isOwner: boolean,
+    startDate: string,
+    lastDate: string,
+  ) {
+    const queryBuilder = this.createQueryBuilder('diary')
+      .leftJoinAndSelect('diary.tags', 'tags')
+      .leftJoinAndSelect('diary.reactions', 'reactions')
+      .where('diary.author.id = :authorId', { authorId })
+      .andWhere('diary.createdAt BETWEEN :startDate AND :lastDate', { startDate, lastDate })
+      .orderBy('diary.id', 'DESC');
+
+    if (!isOwner) {
+      queryBuilder.andWhere('diary.status = :status', { status: 'public' });
+    }
+
+    return queryBuilder.getMany();
   }
 
   async findAllDiaryBetweenDates(
@@ -45,6 +67,8 @@ export class DiariesRepository extends Repository<Diary> {
     lastDate: string,
   ): Promise<Diary[]> {
     const queryBuilder = this.createQueryBuilder('diary')
+      .leftJoinAndSelect('diary.tags', 'tags')
+      .leftJoinAndSelect('diary.reactions', 'reactions')
       .where('diary.author.id = :userId', { userId })
       .andWhere('diary.createdAt BETWEEN :startDate AND :lastDate', { startDate, lastDate });
 
