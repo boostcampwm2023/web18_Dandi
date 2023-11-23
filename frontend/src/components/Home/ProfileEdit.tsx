d3c78d2025d6b50a2402150cce06d675cc6cf891
이도훈 <qwer2854@naver.com>
2023-11-23 14:34:58 +0900

    fix: userId를 사용하지 않아 빌드가 안되는 문제 수정

--- frontend/src/components/Home/ProfileEdit.tsx
+++ frontend/src/components/Home/ProfileEdit.tsx
@@ -5,7 +5,7 @@ interface ProfileEditProps {
 }
 
 const ProfileEdit = () => {
-  const { profileImage, nickname, userId }: ProfileEditProps = {
+  const { profileImage, nickname }: ProfileEditProps = {
     profileImage:
       'https://mblogthumb-phinf.pstatic.net/MjAyMzA1MDZfMjg2/MDAxNjgzMzY5MzE1MTky.eVMofWydN_T-5Cn227nrfcdyPVzpHRN2jaJXGLeVyUUg.S_l9nnV4ANRX4t9isjrt5rbUd8iWyM8D8w6yJMcPktEg.PNG.withwithpet/%EC%8A%A4%ED%81%AC%EB%A6%B0%EC%83%B7_2023-05-06_%EC%98%A4%ED%9B%84_7.25.06.png?type=w800',
     nickname: '단디',
