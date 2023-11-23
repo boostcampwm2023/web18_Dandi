de17c654b931eeeacc2b88a5afb527943196cb0d
이도훈 <qwer2854@naver.com>
2023-11-23 13:50:29 +0900

    faet: 로그인 API 연동 #120
    네이버 로그인 성공 시 redirect 되는 동작을 처리할 페이지 구현

--- frontend/src/components/Login/LoginForm.tsx
+++ frontend/src/components/Login/LoginForm.tsx
@@ -1,4 +1,4 @@
-import { login } from '@api/Login';
+import { goNaverLoginForm } from '@api/Login';
 import logo from '@assets/image/logo.svg';
 import naverLogin from '@assets/image/naverLogin.svg';
 
@@ -7,7 +7,7 @@ const LoginForm = () => {
     <div className="border-default relative flex  w-1/4 flex-col items-center justify-center gap-9 rounded-2xl bg-[#fff] py-16 shadow-[0_0_20px_0_rgba(0,0,0,0.25)]">
       <img src={logo} alt="메인 로고" className="w-1/4" />
       <p className="text-default font-bold">SNS로 빠르게 단디 시작하기</p>
-      <button className="flex w-1/2 justify-center" onClick={login}>
+      <button className="flex w-1/2 justify-center" onClick={goNaverLoginForm}>
         <img src={naverLogin} alt="네이버 로그인 버튼" className="w-4/5 rounded-lg" />
       </button>
     </div>
