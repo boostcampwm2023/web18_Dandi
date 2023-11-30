26cbd344d2a7c93a031424f6e8239ad903027fc4
최수현 <sw92522@naver.com>
2023-11-30 16:31:33 +0900

    feat: swagger annotation 작성 #166

--- backend/src/tags/tags.controller.ts
+++ backend/src/tags/tags.controller.ts
@@ -4,6 +4,7 @@ import { JwtAuthGuard } from 'src/auth/guards/jwtAuth.guard';
 import { User as UserEntity } from 'src/users/entity/user.entity';
 import { RecommendedTagsResponseDto } from './dto/tag.dto';
 import { User } from 'src/users/utils/user.decorator';
+import { ApiOkResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('tags')
 export class TagsController {
@@ -11,6 +12,8 @@ export class TagsController {
 
   @Get('search/:keyword')
   @UseGuards(JwtAuthGuard)
+  @ApiOperation({ description: '추천 키워드 조회 API' })
+  @ApiOkResponse({ description: '추천 키워드 조회 성공' })
   async recommendKeywords(
     @User() user: UserEntity,
     @Param('keyword') keyword: string,
