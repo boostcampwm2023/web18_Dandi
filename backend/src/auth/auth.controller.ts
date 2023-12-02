530a05ea35123d6201b44fc6d7b3588a94664049
최수현 <sw92522@naver.com>
2023-12-02 22:32:54 +0900

    feat: 로그아웃 구현 #211

--- backend/src/auth/auth.controller.ts
+++ backend/src/auth/auth.controller.ts
@@ -11,9 +11,7 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('/login')
-  @ApiOperation({
-    description: 'Oauth 로그인 검증 및 토큰 발급 API',
-  })
+  @ApiOperation({ description: 'Oauth 로그인 검증 및 토큰 발급 API' })
   @ApiOkResponse({ description: '소셜 로그인 성공', type: OAuthLoginResponseDto })
   async oauthLogin(@Body() oauthLoginDto: OAuthLoginDto, @Res() res: Response): Promise<void> {
     const loginResult = await this.authService.login(oauthLoginDto);
@@ -23,14 +21,21 @@ export class AuthController {
   }
 
   @Get('refresh_token')
-  @ApiOperation({
-    description: 'access token 갱신 API',
-  })
+  @ApiOperation({ description: 'access token 갱신 API' })
   @ApiOkResponse({ description: 'access token 갱신 성공' })
   async refreshAccessToken(@Req() req: Request, @Res() res: Response): Promise<void> {
     const newJwt = await this.authService.refreshAccessToken(req);
 
     res.cookie('utk', newJwt, { httpOnly: true });
     res.redirect(SERVICE_URL);
+  }
+
+  @Post('logout')
+  @ApiOperation({ description: '로그아웃 API' })
+  @ApiOkResponse({ description: '로그아웃 성공' })
+  logout(@Res() res: Response): string {
+    res.cookie('utk', '', { maxAge: 0 });
+
+    return '정상적으로 로그아웃되었습니다.';
   }
 }
--- backend/src/auth/utils/auth.constant.ts
+++ backend/src/auth/utils/auth.constant.ts
@@ -4,6 +4,6 @@ export const JWT_EXPIRE_DATE = '1800s';
 // 2주
 export const REFRESH_TOKEN_EXPIRE_DATE = 60 * 60 * 24 * 14;
 
-export const SERVICE_URL = 'http://223.130.146.253/';
+export const SERVICE_URL = 'http://223.130.146.253';
 export const NAVER_OAUTH_URL = 'https://nid.naver.com/oauth2.0/token';
 export const GET_NAVER_PROFILE_URL = 'https://openapi.naver.com/v1/nid/me';
