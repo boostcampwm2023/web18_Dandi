5b8ec0a9e632a267d01d2fd182281a296d010ca2
이도훈 <qwer2854@naver.com>
2023-12-04 03:38:46 +0900

    feat: Protected routers를 사용하여 로그인 여부에 따른 화면 이동 #153
    비로그인 시 로그인 화면으로 이동시키고 로그인 후 다시 로그인 페이지로 이동 시 홈으로 이동

--- frontend/src/Router.tsx
+++ frontend/src/Router.tsx
@@ -8,20 +8,26 @@ import Edit from '@pages/Edit';
 import Detail from '@pages/Detail';
 import AuthLogin from '@pages/OAuthLogin';
 
-import { PAGE_URL } from './util/constants';
+import PrivateRoutes from '@util/PrivateRoutes';
+import AuthRoutes from '@util/AuthRoutes';
+import { PAGE_URL } from '@util/constants';
 
 const Router = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={`${PAGE_URL.HOME}`} element={<Home />} />
-        <Route path={`${PAGE_URL.HOME}/:userId`} element={<Home />} />
-        <Route path={`${PAGE_URL.LOGIN}`} element={<Login />} />
-        <Route path={`${PAGE_URL.FEED}`} element={<Feed />} />
-        <Route path={`${PAGE_URL.MY_DIARY}`} element={<MyDiary />} />
-        <Route path={`${PAGE_URL.EDIT}`} element={<Edit />} />
-        <Route path={`${PAGE_URL.DETAIL}/:diaryId`} element={<Detail />} />
-        <Route path={`${PAGE_URL.AUTH}`} element={<AuthLogin />} />
+        <Route element={<PrivateRoutes />}>
+          <Route path={`${PAGE_URL.HOME}`} element={<Home />} />
+          <Route path={`${PAGE_URL.HOME}/:userId`} element={<Home />} />
+          <Route path={`${PAGE_URL.FEED}`} element={<Feed />} />
+          <Route path={`${PAGE_URL.MY_DIARY}`} element={<MyDiary />} />
+          <Route path={`${PAGE_URL.EDIT}`} element={<Edit />} />
+          <Route path={`${PAGE_URL.DETAIL}/:diaryId`} element={<Detail />} />
+        </Route>
+        <Route element={<AuthRoutes />}>
+          <Route path={`${PAGE_URL.LOGIN}`} element={<Login />} />
+          <Route path={`${PAGE_URL.AUTH}`} element={<AuthLogin />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   );
