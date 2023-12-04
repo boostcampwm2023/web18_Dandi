821bc9d0fe80dad874de564a4f75d58367129332
이도훈 <qwer2854@naver.com>
2023-12-04 19:49:26 +0900

    feat: 로그인 판별식에 undefined 문자열 추가 #240
    로그인 판별식에 undefined 문자열을 추가하여 로그인에 실패해도 홈페이지를 볼 수 있는 문제를 수정하였습니다

--- frontend/src/util/AuthRoutes.tsx
+++ frontend/src/util/AuthRoutes.tsx
@@ -2,8 +2,9 @@ import { Outlet, Navigate } from 'react-router-dom';
 import { PAGE_URL } from '@util/constants';
 
 const AuthRoutes = () => {
-  const isLogin = localStorage.getItem('userId') ? true : false;
-  return !isLogin ? <Outlet /> : <Navigate to={PAGE_URL.HOME} />;
+  const isLogin =
+    localStorage.getItem('userId') && localStorage.getItem('userId') !== 'undefined' ? true : false;
+  return isLogin ? <Navigate to={PAGE_URL.HOME} /> : <Outlet />;
 };
 
 export default AuthRoutes;
