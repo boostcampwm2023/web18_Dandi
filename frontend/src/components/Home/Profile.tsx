ec47116e3271f54754c1a484362f4a8140cb1ccc
이도훈 <qwer2854@naver.com>
2023-12-05 16:09:43 +0900

    feat: 친구 홈 - 로그인한 사용자와의 관계 표시 #251
    로그인한 사용자와의 관계에 따라 친구요청, 끊기가 출력되도록 함

--- frontend/src/components/Home/Profile.tsx
+++ frontend/src/components/Home/Profile.tsx
@@ -1,5 +1,8 @@
 import { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
+import { useMutation, useQueryClient } from '@tanstack/react-query';
+
+import { deleteFriend, requestFriend } from '@/api/FriendModal';
 
 import Button from '@components/Common/Button';
 import Modal from '@components/Common/Modal';
@@ -26,15 +29,26 @@ interface ProfileData {
   profileImage: string;
   totalFriends: number;
   isExistedTodayDiary: boolean;
+  relation: null | relationData;
+}
+
+interface relationData {
+  status: 'complete' | 'waiting';
+  senderId: number;
+  receiverId: number;
 }
 
 const Profile = ({ userId, userData }: ProfileProps) => {
   const navigate = useNavigate();
+  const queryClient = useQueryClient();
+
+  const isFriendHome = useParams().userId ?? false;
 
   const [showModalType, setShowModalType] = useState('list');
   const [showModal, setShowModal] = useState(false);
 
-  const { nickname, profileImage, totalFriends, isExistedTodayDiary }: ProfileData = userData;
+  const { nickname, profileImage, totalFriends, isExistedTodayDiary, relation }: ProfileData =
+    userData;
   const targetPage = isExistedTodayDiary ? PAGE_URL.MY_DIARY : PAGE_URL.EDIT;
 
   const closeModal = () => {
@@ -61,6 +75,54 @@ const Profile = ({ userId, userData }: ProfileProps) => {
 
   const getRandomIndex = Math.floor(Math.random() * GREET_MESSAGES.length);
 
+  const requestFriendMutation = useMutation({
+    mutationFn: (receiverId: number) => requestFriend(receiverId),
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ['sendList'],
+      });
+    },
+  });
+
+  const deleteFriendMutation = useMutation({
+    mutationFn: (friendId: number) => deleteFriend(friendId),
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ['friendList'],
+      });
+    },
+  });
+
+  const getRelationContent = () => {
+    if (relation === null) {
+      return (
+        <p
+          onClick={() => {
+            requestFriendMutation.mutate(+userId);
+          }}
+          className="cursor-pointer"
+        >
+          친구 요청
+        </p>
+      );
+    }
+
+    if (relation.status === 'waiting') {
+      return <p>대기중</p>;
+    }
+
+    return (
+      <p
+        onClick={() => {
+          deleteFriendMutation.mutate(+userId);
+        }}
+        className="cursor-pointer"
+      >
+        친구 끊기
+      </p>
+    );
+  };
+
   return (
     <section className="mb-10 flex flex-col items-center justify-center">
       <div className="mb-10 flex flex-row items-center justify-center">
@@ -73,28 +135,43 @@ const Profile = ({ userId, userData }: ProfileProps) => {
           <p className="mb-8 text-3xl font-bold">
             {nickname}님, {GREET_MESSAGES[getRandomIndex]}
           </p>
-          <div className="border-brown grid w-max grid-flow-col rounded-2xl border-2 border-solid p-5 text-center text-lg font-bold">
-            <p
-              className="cursor-pointer"
-              onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.LIST)}
-            >
-              친구 {totalFriends}명
-            </p>
-            <div className="border-brown mx-5 border-l-2 border-solid" />
-            <p
-              className="cursor-pointer"
-              onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.REQUEST)}
-            >
-              친구 관리
-            </p>
-            <div className="border-brown mx-5 border-l-2 border-solid" />
-            <p
-              className="cursor-pointer"
-              onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.EDIT)}
-            >
-              내 정보 수정
-            </p>
-          </div>
+          {isFriendHome && (
+            <div className="border-brown grid w-max grid-flow-col rounded-2xl border-2 border-solid p-5 text-center text-lg font-bold">
+              <p
+                className="cursor-pointer"
+                onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.LIST)}
+              >
+                친구 {totalFriends}명
+              </p>
+              <div className="border-brown mx-5 border-l-2 border-solid" />
+              {getRelationContent()}
+            </div>
+          )}
+
+          {!isFriendHome && (
+            <div className="border-brown grid w-max grid-flow-col rounded-2xl border-2 border-solid p-5 text-center text-lg font-bold">
+              <p
+                className="cursor-pointer"
+                onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.LIST)}
+              >
+                친구 {totalFriends}명
+              </p>
+              <div className="border-brown mx-5 border-l-2 border-solid" />
+              <p
+                className="cursor-pointer"
+                onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.REQUEST)}
+              >
+                친구 관리
+              </p>
+              <div className="border-brown mx-5 border-l-2 border-solid" />
+              <p
+                className="cursor-pointer"
+                onClick={() => onClickButton(PROFILE_MODAL_CONTENT_TYPE.EDIT)}
+              >
+                내 정보 수정
+              </p>
+            </div>
+          )}
         </div>
       </div>
       <div className="mb-5 text-center text-2xl font-bold leading-relaxed">
