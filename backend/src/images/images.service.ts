e485dce8c1f0551385b8e24bd878e7da4b544ea6
최수현 <sw92522@naver.com>
2023-12-05 23:39:05 +0900

    feat: 이미지 리사이징 로직 추가 #228

--- backend/src/images/images.service.ts
+++ backend/src/images/images.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { ImagesRepository } from './images.repository';
+import * as sharp from 'sharp';
 
 @Injectable()
 export class ImagesService {
@@ -9,10 +10,21 @@ export class ImagesService {
     const today = new Date();
     const middlePath = `${today.getFullYear()}/${today.getMonth() + 1}`;
 
+    file = await this.resizeImage(file);
+
     return await this.imagesRepository.uploadImage(userId, file, middlePath);
   }
 
   async uploadProfileImage(userId: number, file: Express.Multer.File) {
+    file = await this.resizeImage(file);
+
     return await this.imagesRepository.uploadImage(userId, file, 'profile');
+  }
+
+  private async resizeImage(file: Express.Multer.File) {
+    file.buffer = await sharp(file.buffer).jpeg({ mozjpeg: true, quality: 80 }).toBuffer();
+    file.originalname = file.originalname.split('.')[0];
+
+    return file;
   }
 }
