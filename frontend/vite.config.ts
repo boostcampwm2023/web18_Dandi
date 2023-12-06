6fd992a42aa0f8243f0a6c89337f16b211066d51
이도훈 <qwer2854@naver.com>
2023-12-07 03:06:21 +0900

    feat: PWA 설치 #284
    PWA를 설치하고 동작까지 확인하였습니다.

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,10 +1,43 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react-swc';
 import { resolve } from 'path';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        name: 'Dandi',
+        short_name: 'Dandi',
+        description: '일기를 쓰며 잔디를 심자!',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/pwa-192x192-maskable.png',
+            sizes: '192x192',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
