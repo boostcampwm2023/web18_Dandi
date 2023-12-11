c7cd490ab36caa38c8ff34216dd3480d3013d407
gimewn <yoonju_1120@naver.com>
2023-12-11 17:30:16 +0900

    chore: 사용하지 않는 라이브러리 Import 제거

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useRef, useState } from 'react';
+import { useEffect, useRef } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useInfiniteQuery, useQuery } from '@tanstack/react-query';
 
