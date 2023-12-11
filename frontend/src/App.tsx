3b2a68e61952438c9c97755071478b96fc7d0eac
이도훈 <qwer2854@naver.com>
2023-12-11 21:50:55 +0900

    feat: Toast 구현 #322
    contextAPI를 사용하여 Toast를 구현하였습니다.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,11 +1,14 @@
 import Router from './Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
+import { ToastProvider } from '@/util/ToastProvider';
 
 const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Router />
+      <ToastProvider>
+        <Router />
+      </ToastProvider>
     </QueryClientProvider>
   );
 }
