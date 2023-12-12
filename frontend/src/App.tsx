1459cac2a1b5761a6695012e6ae3136282c844e4
gimewn <yoonju_1120@naver.com>
2023-12-12 17:05:36 +0900

    chore: App util 경로 수정

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import Router from './Router';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ToastProvider } from '@util/ToastProvider';
-import ModalProvider from './util/ModalProvider';
+import ModalProvider from '@util/ModalProvider';
 
 const queryClient = new QueryClient();
 function App() {
