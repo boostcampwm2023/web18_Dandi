fc9be976a8116dc35f4aac8643d775fc2347c677
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-01-08 20:42:32 +0900

    test: 친구 신청 취소 로직 테스트 작성 #364

--- backend/test/service/friendsService.test.ts
+++ backend/test/service/friendsService.test.ts
@@ -100,3 +100,134 @@ describe('친구 요청 테스트', () => {
     expect(friendsRepository.createFriend).toHaveBeenCalledTimes(1);
   });
 });
+
+describe('친구 신청 취소 테스트', () => {
+  let friendsService: FriendsService;
+  let friendsRepository: FriendsRepository;
+
+  beforeAll(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        FriendsService,
+        FriendsRepository,
+        UsersRepository,
+        { provide: dataSource.DataSource, useValue: {} },
+      ],
+    }).compile();
+
+    friendsService = module.get<FriendsService>(FriendsService);
+    friendsRepository = module.get<FriendsRepository>(FriendsRepository);
+  });
+
+  beforeEach(() => jest.clearAllMocks());
+
+  it('요청ID와 수신ID가 일치하면 예외 발생', () => {
+    //given
+    const friendsRelationDto = { senderId: 1, receiverId: 1 };
+
+    //when - then
+    expect(() => friendsService.cancelFriendRequest(friendsRelationDto)).rejects.toThrow(
+      new BadRequestException('나와는 친구신청 관리를 할 수 없습니다.'),
+    );
+    expect(friendsRepository.findFriendRequest).toHaveBeenCalledTimes(0);
+  });
+
+  it('수신한 친구 요청이 존재하면 예외 발생', async () => {
+    //given
+    const friendsRelationDto = { senderId: 1, receiverId: 2 };
+    const mockFriends = {
+      id: 1,
+      status: FriendStatus.WAITING,
+      sender: { id: 2 },
+      receiver: { id: 1 },
+    };
+    (friendsRepository.findFriendRequest as jest.Mock).mockImplementation(
+      (senderId: number, receiverId: number) => {
+        if (senderId === 2) {
+          return mockFriends;
+        }
+        return null;
+      },
+    );
+
+    //when - then
+    await expect(
+      async () => await friendsService.cancelFriendRequest(friendsRelationDto),
+    ).rejects.toThrow(new BadRequestException('상대의 친구신청을 확인하세요.'));
+    expect(friendsRepository.findFriendRequest).toHaveBeenCalledTimes(2);
+  });
+
+  it('수신한 친구 요청이 존재하면서, 이미 친구라면 예외 발생', async () => {
+    //given
+    const friendsRelationDto = { senderId: 1, receiverId: 2 };
+    const mockFriends = {
+      id: 1,
+      status: FriendStatus.COMPLETE,
+      sender: { id: 2 },
+      receiver: { id: 1 },
+    };
+    (friendsRepository.findFriendRequest as jest.Mock).mockImplementation(
+      (senderId: number, receiverId: number) => {
+        if (senderId === 2) {
+          return mockFriends;
+        }
+        return null;
+      },
+    );
+
+    //when - then
+    await expect(
+      async () => await friendsService.cancelFriendRequest(friendsRelationDto),
+    ).rejects.toThrow(new BadRequestException('이미 친구입니다.'));
+    expect(friendsRepository.findFriendRequest).toHaveBeenCalledTimes(2);
+  });
+
+  it('친구 신청 기록이 존재하지 않으면 예외 발생', async () => {
+    //given
+    const friendsRelationDto = { senderId: 1, receiverId: 2 };
+    (friendsRepository.findFriendRequest as jest.Mock).mockReturnValue(null);
+
+    //when - then
+    await expect(
+      async () => await friendsService.cancelFriendRequest(friendsRelationDto),
+    ).rejects.toThrow(new BadRequestException('해당 사용자 사이의 친구신청 기록이 없습니다.'));
+    expect(friendsRepository.findFriendRequest).toHaveBeenCalledTimes(2);
+  });
+
+  it('송신한 친구 요청이 존재하지만, 이미 친구 관계라면 예외 발생', async () => {
+    //given
+    const friendsRelationDto = { senderId: 1, receiverId: 2 };
+    const mockFriends = {
+      id: 1,
+      status: FriendStatus.COMPLETE,
+      sender: { id: 1 },
+      receiver: { id: 2 },
+    };
+    (friendsRepository.findFriendRequest as jest.Mock).mockResolvedValue(mockFriends);
+
+    //when - then
+    await expect(
+      async () => await friendsService.cancelFriendRequest(friendsRelationDto),
+    ).rejects.toThrow(new BadRequestException('이미 친구입니다.'));
+    expect(friendsRepository.findFriendRequest).toHaveBeenCalledTimes(1);
+  });
+
+  it('송신한 친구 신청 기록이 존재하면 기록 삭제', async () => {
+    //given
+    const friendsRelationDto = { senderId: 1, receiverId: 2 };
+    const mockFriends = {
+      id: 1,
+      status: FriendStatus.WAITING,
+      sender: { id: 2 },
+      receiver: { id: 1 },
+    };
+    (friendsRepository.findFriendRequest as jest.Mock).mockReturnValue(mockFriends);
+
+    //when
+    await friendsService.cancelFriendRequest(friendsRelationDto);
+
+    //then
+    expect(friendsRepository.findFriendRequest).toHaveBeenCalledTimes(1);
+    expect(friendsRepository.removeRelation).toHaveBeenCalledTimes(1);
+  });
+});
