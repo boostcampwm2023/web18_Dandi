9416bbbf391830cb98d0edac49dd530da273d89a
unknown <speed04313@gmail.com>
2024-01-15 23:07:47 +0900

    feat: useGrassQuery 커스텀 훅 추가 #373
    쿼리 키로 grass를 사용하는 useQuery를 커스텀 훅으로 분리

---
+++ frontend/src/hooks/useGrassQuery.ts
@@ -0,0 +1,14 @@
+import { useQuery } from '@tanstack/react-query';
+
+import getGrass from '@api/Grass';
+import { reactQueryKeys } from '@util/constants';
+
+const useGrassQuery = (userId: string) => {
+  return useQuery({
+    queryKey: [reactQueryKeys.Grass, userId],
+    queryFn: () => getGrass(Number(userId)),
+    staleTime: Infinity,
+  });
+};
+
+export default useGrassQuery;
