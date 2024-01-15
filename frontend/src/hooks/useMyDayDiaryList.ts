fe728d60f8d1ed8b638cdf7caf6f5634792d08d0
unknown <speed04313@gmail.com>
2024-01-15 23:27:06 +0900

    feat: useMyDayDiaryList 커스텀 훅 추가 #373
    myDayDiaryList를 받아오는 커스텀 훅 추가
    useDayDiaryList와 쿼리 키를 제외하고 같기 때문에 추후 수정할 예정

---
+++ frontend/src/hooks/useMyDayDiaryList.ts
@@ -0,0 +1,36 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+import { getDiaryDayList } from '@api/DiaryList';
+import { InfiniteDiaryListProps } from '@type/components/Common/DiaryList';
+import { viewTypes } from '@type/pages/MyDiary';
+import { reactQueryKeys, DIARY_VIEW_TYPE_LIST } from '@util/constants';
+
+const useMyDayDiaryList = (userId: string) => {
+  return useInfiniteQuery<
+    any,
+    Error,
+    InfiniteDiaryListProps,
+    [string, string | null],
+    { userId: string; type: viewTypes; lastIndex: number }
+  >({
+    queryKey: [reactQueryKeys.MyDayDiaryList, userId],
+
+    queryFn: getDiaryDayList,
+    initialPageParam: {
+      userId: userId,
+      type: DIARY_VIEW_TYPE_LIST[0],
+      lastIndex: 2e9,
+    },
+    getNextPageParam: (lastPage) => {
+      return lastPage && lastPage.diaryList.length >= 5
+        ? {
+            userId: userId,
+            type: DIARY_VIEW_TYPE_LIST[0],
+            lastIndex: lastPage?.diaryList.at(-1).diaryId,
+          }
+        : undefined;
+    },
+  });
+};
+
+export default useMyDayDiaryList;
