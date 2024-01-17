a83f346a2d867c9d4bc8bab2c14d2c258518f7a8
unknown <speed04313@gmail.com>
2024-01-17 11:58:21 +0900

    feat: userMyWeekDiary 커스텀 훅 분리 #373
    myWeekDiary를 받아오는 useQuery를 커스텀 훅으로 분리

---
+++ frontend/src/hooks/useMyWeekDiary.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getDiaryWeekAndMonthList } from '@api/DiaryList';
+import { IDiaryContent } from '@type/components/Common/DiaryList';
+import { reactQueryKeys } from '@util/constants';
+import { formatDateDash } from '@util/funcs';
+
+const useMyWeekDiary = (userId: string, period: Date[]) => {
+  return useQuery<{ nickname: string; diaryList: IDiaryContent[] }>({
+    queryKey: [
+      reactQueryKeys.MyWeekDiary,
+      userId,
+      formatDateDash(period[0]),
+      formatDateDash(period[1]),
+    ],
+    queryFn: () =>
+      getDiaryWeekAndMonthList({
+        userId: userId,
+        type: 'Week',
+        startDate: formatDateDash(period[0]),
+        endDate: formatDateDash(period[1]),
+      }),
+  });
+};
+
+export default useMyWeekDiary;
