77238a76af1940bad225db157be9a3c96d5bc347
unknown <speed04313@gmail.com>
2024-01-17 11:54:24 +0900

    feat: useMonthDiaryData 커스텀 훅 분리 #373
    monthDiaryData를 받아오는 useQuery 커스텀 훅 분리

---
+++ frontend/src/hooks/useMonthDiaryData.ts
@@ -0,0 +1,31 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { getDiaryWeekAndMonthList } from '@api/DiaryList';
+import { IDiaryContent } from '@type/components/Common/DiaryList';
+import { EmotionData } from '@type/components/MyDiary/MonthContainer';
+import { reactQueryKeys } from '@util/constants';
+import { formatDateDash } from '@util/funcs';
+
+const useMonthDiaryData = (userId: string, nowMonth: Date, first: Date, last: Date) => {
+  return useQuery({
+    queryKey: [reactQueryKeys.MonthDiaryData, userId, formatDateDash(nowMonth)],
+    queryFn: () =>
+      getDiaryWeekAndMonthList({
+        userId: userId,
+        type: 'Month',
+        startDate: formatDateDash(first),
+        endDate: formatDateDash(last),
+      }),
+    select: (data) => {
+      const emotionObject: EmotionData = {};
+      data.diaryList.forEach((diary: IDiaryContent) => {
+        const { diaryId, emotion, createdAt } = diary;
+        const day = new Date(createdAt).getDate();
+        emotionObject[day] = { diaryId: +diaryId, emotion: emotion };
+      });
+      return emotionObject;
+    },
+  });
+};
+
+export default useMonthDiaryData;
