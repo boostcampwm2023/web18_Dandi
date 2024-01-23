6a5faf82fad6b6d4e6cae4d891f073a82c3105c7
unknown <speed04313@gmail.com>
2024-01-23 15:48:13 +0900

    fix: 내 일기의 키워드 검색 CSS 수정 #380
    내 일기에서 공감 목록 모달을 띄울 시, 키워드 검색 칸이 모달 위로 보이는
    오류 수정

--- frontend/src/types/pages/MyDiary.ts
+++ frontend/src/types/pages/MyDiary.ts
@@ -1,5 +1,5 @@
 export type viewTypes = 'Day' | 'Week' | 'Month';
-export type searchOptionsType = '키워드' | '제목 + 내용';
+export type searchOptionsType = '키워드' | '제목+내용';
 
 export const isViewTypes = (type: string): type is viewTypes => {
   return type === 'Day' || type === 'Week' || type === 'Month';
