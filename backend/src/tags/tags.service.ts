4dbff8e6213965ac46e73e0cab78b5a11e253d7a
rio <75190035+HyoJongPark@users.noreply.github.com>
2024-01-25 17:45:52 +0900

    test: 데이터가 존재할 때 tag 키워드 조회 테스트 작성 #328

--- backend/src/tags/tags.service.ts
+++ backend/src/tags/tags.service.ts
@@ -21,17 +21,17 @@ export class TagsService {
   }
 
   async updateDataSetScore(userId: number, tagNames: string[]) {
-    Promise.all([
-      tagNames.forEach(async (tag) => {
-        const tagScore = await this.tagsRepository.zscore(`${userId}`, tag);
+    const promises = tagNames.map(async (tag) => {
+      const tagScore = await this.tagsRepository.zscore(`${userId}`, tag);
 
-        if (!tagScore) {
-          this.tagsRepository.zadd(`${userId}`, tag); // 데이터셋에 추가
-        } else {
-          this.tagsRepository.zincrby(`${userId}`, tag); // 점수 +1
-        }
-      }),
-    ]);
+      if (!tagScore) {
+        this.tagsRepository.zadd(`${userId}`, tag); // 데이터셋에 추가
+      } else {
+        this.tagsRepository.zincrby(`${userId}`, tag); // 점수 +1
+      }
+    });
+
+    Promise.all(promises);
   }
 
   async recommendKeywords(userId: number, keyword: string) {
