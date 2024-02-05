d82f404af1663c7480f0bb56630dd87ef075eae7
최수현 <sw92522@naver.com>
2024-02-05 14:27:33 +0900

    test: 친구목록 조회 API 테스트 #832

--- backend/test/friends/friends.e2e-spec.ts
+++ backend/test/friends/friends.e2e-spec.ts
@@ -7,10 +7,13 @@ import { DataSource, QueryRunner } from 'typeorm';
 import { TestAuthGuard } from 'test/utils/testAuthGuard';
 import { UsersRepository } from 'src/users/users.repository';
 import { SocialType } from 'src/users/entity/socialType';
+import { FriendsRepository } from 'src/friends/friends.repository';
+import { FriendStatus } from 'src/friends/entity/friendStatus';
 
 describe('FriendsController (e2e)', () => {
   let app: INestApplication;
   let queryRunner: QueryRunner;
+  let friendsRepository: FriendsRepository;
   let usersRepository: UsersRepository;
 
   beforeAll(async () => {
@@ -32,6 +35,7 @@ describe('FriendsController (e2e)', () => {
     queryRunner.release = jest.fn();
     (dataSource.createQueryRunner as jest.Mock).mockReturnValue(queryRunner);
 
+    friendsRepository = module.get<FriendsRepository>(FriendsRepository);
     usersRepository = module.get<UsersRepository>(UsersRepository);
     app = module.createNestApplication();
     await app.init();
@@ -41,24 +45,72 @@ describe('FriendsController (e2e)', () => {
     await app.close();
   });
 
-  it('test', async () => {
-    await queryRunner.startTransaction();
-    const user = await usersRepository.createUser(
-      {
-        id: '1234',
-        nickname: 'test',
-        email: 'test@abc.com',
-        profile_image: 'profile image',
-      },
-      SocialType.NAVER,
-    );
+  const userInfo = {
+    socialId: '1234',
+    socialType: SocialType.NAVER,
+    nickname: 'test',
+    email: 'test@abc.com',
+    profileImage: 'profile image',
+  };
+  const friend1Info = {
+    socialId: '1',
+    socialType: SocialType.NAVER,
+    nickname: 'friend1',
+    email: 'friend1@abc.com',
+    profileImage: 'profile image',
+  };
+  const friend2Info = {
+    socialId: '2',
+    socialType: SocialType.NAVER,
+    nickname: 'friend2',
+    email: 'friend2@abc.com',
+    profileImage: 'profile image',
+  };
 
-    const res = await request(app.getHttpServer())
-      .post('/')
-      .use(() => new TestAuthGuard(user));
+  describe('/friends/:userId (GET)', () => {
+    beforeEach(async () => {
+      await queryRunner.startTransaction();
+    });
 
-    expect(res.statusCode).toEqual(404);
+    afterEach(async () => {
+      await queryRunner.rollbackTransaction();
+    });
 
-    await queryRunner.rollbackTransaction();
+    it('친구목록 조회, 친구가 없는 경우 빈 배열 반환', async () => {
+      // given
+      const user = await usersRepository.save(userInfo);
+      const url = `/friends/${user.id}`;
+
+      // when
+      const response = await request(app.getHttpServer())
+        .get(url)
+        .use(() => new TestAuthGuard(user));
+
+      // then
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.friends).toEqual([]);
+    });
+
+    it('친구 목록 정상 조회', async () => {
+      // given
+      const user = await usersRepository.save(userInfo);
+      const url = `/friends/${user.id}`;
+
+      const friend1 = await usersRepository.save(friend1Info);
+      const friend2 = await usersRepository.save(friend2Info);
+      const relation1 = await friendsRepository.save({ sender: user, receiver: friend1 });
+      const relation2 = await friendsRepository.save({ sender: friend2, receiver: user });
+      await friendsRepository.update(relation1.id, { status: FriendStatus.COMPLETE });
+      await friendsRepository.update(relation2.id, { status: FriendStatus.COMPLETE });
+
+      // when
+      const response = await request(app.getHttpServer())
+        .get(url)
+        .use(() => new TestAuthGuard(user));
+
+      // then
+      expect(response.statusCode).toEqual(200);
+      expect(response.body.friends).toHaveLength(2);
+    });
   });
 });
