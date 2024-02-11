e46f1001a06d6220dcd260f2d7f268f7f038804d
최수현 <sw92522@naver.com>
2024-02-12 01:12:43 +0900

    test: 사용자 정보 수정 API 테스트 작성 #382

--- backend/test/users/users.e2e-spec.ts
+++ backend/test/users/users.e2e-spec.ts
@@ -5,6 +5,9 @@ import { AppModule } from 'src/app.module';
 import { DataSource, QueryRunner } from 'typeorm';
 import * as cookieParser from 'cookie-parser';
 import { UsersRepository } from 'src/users/users.repository';
+import { User } from 'src/users/entity/user.entity';
+import { SocialType } from 'src/users/entity/socialType';
+import { testLogin } from 'test/utils/testLogin';
 
 describe('UsersController (e2e)', () => {
   let app: INestApplication;
@@ -39,5 +42,88 @@ describe('UsersController (e2e)', () => {
 
   afterEach(async () => {
     await queryRunner.rollbackTransaction();
+  });
+
+  describe('/users (PATCH)', () => {
+    const url = '/users';
+    let user: User;
+    let accessToken: string;
+
+    beforeEach(async () => {
+      const userInfo = {
+        socialId: '1234',
+        socialType: SocialType.NAVER,
+        nickname: 'test',
+        email: 'test@abc.com',
+        profileImage: 'profile image',
+      };
+
+      user = await usersRepository.save(userInfo);
+      accessToken = await testLogin(user);
+    });
+
+    it('프로필 사진과 사용자 닉네임 수정', async () => {
+      // given
+      const imageFilePath = './test/testImage.png';
+
+      // when
+      const response = await request(app.getHttpServer())
+        .patch(url)
+        .set('Cookie', [`utk=${accessToken}`])
+        .field('nickname', '테스트')
+        .attach('profileImage', imageFilePath);
+
+      // then
+      expect(response.statusCode).toEqual(200);
+    });
+
+    it('프로필 사진만 수정', async () => {
+      // given
+      const imageFilePath = './test/testImage.png';
+
+      // when
+      const response = await request(app.getHttpServer())
+        .patch(url)
+        .set('Cookie', [`utk=${accessToken}`])
+        .attach('profileImage', imageFilePath);
+
+      // then
+      expect(response.statusCode).toEqual(200);
+    });
+
+    it('사용자 닉네임만 수정', async () => {
+      // when
+      const response = await request(app.getHttpServer())
+        .patch(url)
+        .set('Cookie', [`utk=${accessToken}`])
+        .field('nickname', '테스트');
+
+      // then
+      expect(response.statusCode).toEqual(200);
+    });
+
+    it('수정할 프로필 사진의 파일 형식이 옳바르지 않은 경우 예외 발생', async () => {
+      // given
+      const filePath = './test/users/testFile.txt';
+
+      // when
+      const response = await request(app.getHttpServer())
+        .patch(url)
+        .set('Cookie', [`utk=${accessToken}`])
+        .attach('profileImage', filePath);
+
+      // then
+      expect(response.statusCode).toEqual(400);
+    });
+
+    it('수정할 정보가 없는 경우 예외 발생', async () => {
+      // when
+      const response = await request(app.getHttpServer())
+        .patch(url)
+        .set('Cookie', [`utk=${accessToken}`]);
+
+      // then
+      expect(response.statusCode).toEqual(400);
+    });
   });
 });
